af152706237ce3e7aba643f7fa486303a9d1977d
mersycle <mersycle@hotmail.com>
2017-09-17 23:39:10 +0800

    add py5

--- py5_ListComprehensions.py
+++ py5_ListComprehensions.py
@@ -13,7 +13,40 @@
 # 但如果要生成[1x1, 2x2, 3x3, ..., 10x10]怎么做？方法一是循环：
 L = []
 for x in range(1,11):
-	L.append(x)
+	L.append(x*x)
 
 # 但是循环太繁琐，而列表生成式则可以用一行语句代替循环生成上面的list：
+# 在列表中写出要生成列表的规则
+# 写列表生成式时，把要生成的元素x * x放到前面，后面跟for循环，就可以把list创建出来
+L = [x*x for x in range(1,11)]
 
+# for循环后面还可以加上if判断
+L = [x*x for x in range(1,11) if x%2==0]
+
+
+# 还可以使用两层循环，可以生成全排列
+L = [n+m for n in "ABC" for m in '123']
+# print(L) # ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+
+
+# 列出当前目录下的所有文件和目录名
+import os
+dirname = [d for d in os.listdir('.')]
+# print(dirname)
+
+
+# 列表生成式也可以使用两个变量来生成list
+d = {'a':"A",'b':"B",'c':"C"}
+L = [a+'->'+b for a,b in d.items()]
+# print(L)
+
+
+# 把一个list中所有的字符串变成小写
+L = ['Hello', 'World', 'IBM', 'Apple']
+L = [a.lower() for a in L]
+# print(L)
+
+
+L = ['Hello', 'World', 'IBM', 'Apple',18,None]
+L = [s.lower() for s in L if isinstance(s,str)]
+print(L)
